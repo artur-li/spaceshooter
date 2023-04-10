c59709e8c35413bd196c5805b0d940e9a3dbc3d0
Artur <arturli.official@gmail.com>
2023-04-10 17:53:12 -0400

    created game states and score

--- main.py
+++ main.py
@@ -1,167 +1,160 @@
 import pygame, sys, random
-from datetime import datetime
-
-# general 
 pygame.init()
-font = pygame.font.Font(None, 50)
-font2 = pygame.font.Font(None, 30)
-start_screen = True
-game = False
-game_over = False
-score = 0
 
-# display screen set up
-screen = pygame.display.set_mode((800, 400))
+# screen
+screen = pygame.display.set_mode((500,700))
 clock = pygame.time.Clock()
 
-# play screen
-play_surf = font.render("PLAY", False, "Grey")
-play_rect = play_surf.get_rect(center=(400,200))
-
-# spaceship class(sprite + movement + shooting)
-count = 0
-bullets_shot = []
-class Spaceship(pygame.sprite.Sprite):
+# score
+score = 0
+font = pygame.font.Font(None, 50)
+def update_score():
+    score_surf = font.render("SCORE: " + str(score), False, "grey", "black")
+    score_rect = score_surf.get_rect(center=(250,100))
+    screen.blit(score_surf,score_rect)
+    
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = font.render("A", False, "Green")
-        self.rect = self.image.get_rect(center=(400, 380))
-    def update(self):
-        # movement
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] and self.rect.centerx < 790:
-            self.rect.centerx += 7.5
-        elif keys[pygame.K_LEFT] and self.rect.centerx > 10:
-            self.rect.centerx -= 7.5
-        # shooting
-        global count 
-        if count != 0:
-            count -= 1
-        if keys[pygame.K_SPACE]:
-                if count == 0:
-                    bullet = Bullet(self.rect.centerx, self.rect.centery)
-                    bullets_shot.append(bullet)
-                    spaceship_group.add(bullet)
-                    count = 30
-        # collisions with enemy
-        global score
-        for bullet in bullets_shot:
-            collided = pygame.sprite.spritecollide(bullet, enemy_group, True)
-            for i in collided:
-                score += 1
-
-# bullet class(sprite + movement)
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos):
-        super().__init__()
-        self.image = pygame.surface.Surface((5, 10))
+        self.image = pygame.Surface((30,30))
         self.image.fill("white")
-        self.rect = self.image.get_rect(center=(x_pos, y_pos))
+        self.rect = self.image.get_rect(center=(250,600))
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.centerx >= 25:
+            self.rect.centerx -= 5
+        elif keys[pygame.K_RIGHT] and self.rect.centerx <= 475:
+            self.rect.centerx += 5
+        elif keys[pygame.K_UP] and self.rect.centery >= 25:
+            self.rect.centery -= 5
+        elif keys[pygame.K_DOWN] and self.rect.centery <= 675:
+            self.rect.centery += 5
+    def collision(self):
+        global game_state
+        global over_state
+        collided = pygame.sprite.spritecollide(player, obstacle_group, False)
+        for i in collided:
+            if i != None:
+                game_state = False
+                over_state = True
     def update(self):
-        self.rect.centery -= 5
+        self.movement()
+        self.collision()
+player_group = pygame.sprite.Group()
+player = Player()
+player_group.add(player)
 
-# enemy class(sprite + movement)
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+class Obstacle1(pygame.sprite.Sprite):
+    def __init__(self, x_pos):
         super().__init__()
-        self.image = pygame.Surface((40,40))
+        self.image = pygame.Surface((400,20))
         self.image.fill("red")
-        self.rect = self.image.get_rect(center=(random.randint(20,780),0))
+        self.rect = self.image.get_rect(center=(x_pos,0))
+    def movement(self):
+        self.rect.centery += 2
+    def destruction(self):
+        global score
+        if self.rect.centery > 710:
+            pygame.sprite.Sprite.kill(self)
+            score += 1
     def update(self):
-        global game_over
-        self.rect.y += 2
-        if self.rect.y > 380:
-            game_over = True
-# (enemy spawning)
-spawn_amount = random.randint(1,2)
-count2 = 0
-interval = random.randint(120,300)
+        self.movement()
+        self.destruction()
+class Obstacle2(pygame.sprite.Sprite):
+    def __init__(self, x_pos):
+        super().__init__()
+        self.image = pygame.Surface((20,20))
+        self.image.fill("red")
+        self.rect = self.image.get_rect(center=(x_pos,0))
+    def movement(self):
+        self.rect.centery += 2
+    def destruction(self):
+        if self.rect.centery > 750:
+            pygame.sprite.Sprite.kill(self)
+    def update(self):
+        self.movement()
+        self.destruction()
+timer = 180
+def spawn_obstacles():
+    global timer
+    timer += 1
+    if timer % 120 == 0:
+        x_pos_list = [150, 350]
+        obstacle = Obstacle1(random.choice(x_pos_list))
+        obstacle_group.add(obstacle)
+    elif timer % 60 == 0 and timer % 120 != 0:
+        x_pos_list = [50,100,150,200,250,300,350,400,450]
+        obstacle_a = Obstacle2(random.choice(x_pos_list))
+        obstacle_group.add(obstacle_a)
+        obstacle_b = Obstacle2(random.choice(x_pos_list))
+        obstacle_group.add(obstacle_b)
+        obstacle_c = Obstacle2(random.choice(x_pos_list))
+        obstacle_group.add(obstacle_c)
+obstacle_group = pygame.sprite.Group()
 
-def enemy_spawning():
-    global spawn_amount
-    global count2
-    global interval
-    count2 += 1
-    if count2 % interval == 0:
-        if spawn_amount == 1:
-            enemy = Enemy()
-            enemy_group.add(enemy)
-            count2 = 0
-            interval = random.randint(120,300)
-            spawn_amount = random.randint(1,2)
-        elif spawn_amount == 2:
-            enemy = Enemy()
-            enemy2 = Enemy()
-            enemy_group.add(enemy)
-            enemy_group.add(enemy2)
-            count2 = 0
-            interval = random.randint(120,300)
-            spawn_amount = random.randint(1,2)
+# start_game state
+def start_game():
+    global game_state
+    global start_state
+    play_surf = font.render("PLAY", False, "Blue", "black")
+    play_rect = play_surf.get_rect(center=(250,350))
+    screen.blit(play_surf,play_rect)
+    mouse_pos = pygame.mouse.get_pos()
+    if play_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed() != (False, False, False):
+        start_state = False
+        game_state = True
 
-# (groups and sprites) -init
-# spaceship
-spaceship = Spaceship()
-spaceship_group = pygame.sprite.Group()
-spaceship_group.add(spaceship)
-# enemies
-enemy = Enemy()
-enemy_group = pygame.sprite.Group()
-enemy_group.add(enemy)
+# game_active state
+def game():
+    player_group.draw(screen)
+    spawn_obstacles()
+    obstacle_group.draw(screen)
+    # update
+    player_group.update()
+    obstacle_group.update()
+    # score
+    update_score()
+
+# game_over state
+def game_over():
+    global game_state
+    global score
+    score_surf = font.render("FINAL SCORE: " + str(score), False, "grey", "black")
+    score_rect = score_surf.get_rect(center=(250,320))
+    screen.blit(score_surf,score_rect)
+    play_surf = font.render("PLAY AGAIN", False, "Blue", "black")
+    play_rect = play_surf.get_rect(center=(250,380))
+    screen.blit(play_surf,play_rect)
+    mouse_pos = pygame.mouse.get_pos()
+    if play_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed() != (False, False, False):
+        game_state = True
+        score = 0
+        obstacle_group.empty()
+
+# state variables
+start_state = True
+game_state = False
+over_state = False
 
 # game loop
 while True:
+    screen.fill("black")
 
-    # for event in events
     for event in pygame.event.get():
-        # close if event == "x" 
+        # close screen upon 'x'
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit() 
 
-    # start_screen state
-    if start_screen:
-        screen.blit(play_surf, play_rect)
-        if play_rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed() != (False, False, False):
-            start_screen = False
-            game = True
+    if start_state:
+        start_game()
 
-    # game_running state
-    elif game:
+    elif game_state:
+        game()
 
-        # if game over then end game
-        if game_over:
-            game = False
+    elif over_state:
+        game_over()
 
-        # enemy spawning function
-        enemy_spawning()
-
-        # draw to screen
-        screen.fill("black")
-        score_surf = font2.render("Score: " + str(score), False, "Grey")
-        score_rect = score_surf.get_rect(center=(400,50))
-        screen.blit(score_surf, score_rect)
-        spaceship_group.draw(screen)
-        enemy_group.draw(screen)
-
-        # update the screen 60fps
-        spaceship_group.update()
-        enemy_group.update()
-
-    # game_over state
-    elif game_over:
-        screen.fill("black")
-        score2_surf = font.render("Final Score: " + str(score), False, "Grey")
-        score2_rect = score2_surf.get_rect(center=(400,180))
-        screen.blit(score2_surf, score2_rect)
-        play_again_surf = font.render("play again", False, "Blue")
-        play_again_rect = play_again_surf.get_rect(center=(400,220))
-        screen.blit(play_again_surf, play_again_rect)
-        if play_again_rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed() != (False, False, False):
-            enemy_group.empty()
-            game_over = False
-            game = True
-            score = 0
-
-
+    # update screen 60fps
     pygame.display.update()
     clock.tick(60)
